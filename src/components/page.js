897a5a456ebeebbbb497d183b8a273d84f3e32e0
Bradley Staples <the.bradley.staples@gmail.com>
2021-09-14 23:08:21 -0700

    https for google font link

--- src/components/page.js
+++ src/components/page.js
@@ -15,7 +15,7 @@ const Page = ({ title, children }) => {
         <link rel='shortcut' href='/favicon.ico' />
         <meta name='robots' content='index, follow' />
         <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no' />
-        <link href='http://fonts.googleapis.com/css?family=Arapey' rel='stylesheet' type='text/css' />
+        <link href='https://fonts.googleapis.com/css?family=Arapey' rel='stylesheet' type='text/css' />
       </Helmet>
       {children}
     </div>
